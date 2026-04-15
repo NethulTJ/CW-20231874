88e50eec2b442bf8f6340db4facb9b3adaa079a2
NethulTJ <nethultrevor@gmail.com>
2026-04-16 01:16:15 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/cw/RestApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cw;
+
+/**
+ *
+ * @author Nethul Jayasuriya
+ */
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+public class RestApplication {
+    
+}
