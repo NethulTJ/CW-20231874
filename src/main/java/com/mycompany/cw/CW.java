4824655753c08c96c267b2c374e655b7f3c4a0c6
NethulTJ <nethultrevor@gmail.com>
2026-04-20 11:34:22 +0530

    main CW to call the server

--- src/main/java/com/mycompany/cw/CW.java
+++ src/main/java/com/mycompany/cw/CW.java
@@ -1,16 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.cw;
 
-/**
- *
- * @author Nethul Jayasuriya
- */
-public class CW {
+import java.io.IOException;
+import java.net.URI;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class CW {
+    private static final String BASE_URI = "http://localhost:8080/";
+
+    public static HttpServer startServer() {
+        ResourceConfig config = ResourceConfig.forApplication(new RestApplication());
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = startServer();
+        System.out.println("Smart Campus API running at " + BASE_URI + "api/v1");
+        System.out.println("Press Enter to stop the server.");
+        System.in.read();
+        server.shutdownNow();
     }
 }
+
